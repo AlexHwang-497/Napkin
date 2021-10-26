9f66cfe758d10240ff2ecd2e6942b446fc27b2f0
alex <alexhwang01234@gmail.com>
2021-10-26 14:31:15 -0700

    31.  included seach tags and ability to delete the search tags in the search textbox

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -7,7 +7,7 @@ import Form from '../Components/Forms/Form';
 import {getPosts} from '../actions/posts'
 import Pagination from '../Components/Pagination';
 import useStyles from './styles';
-
+import ChipInput from 'material-ui-chip-input';
 // *this will provide us an idea of where our current location is 
 function useQuery() {
     return new URLSearchParams(useLocation().search);
@@ -23,10 +23,31 @@ const Home = () =>{
     // *this is going to read our url and see if we have a page parameter in it; if so it will poopulate the variable
     const page = query.get('page') || 1;
     const searchQuery = query.get('searchQuery');
+    const [search, setSearch] = useState('');
+    const [tags, setTags] = useState([]);
 
     useEffect(()=> {
         dispatch(getPosts())
     },[currentId,dispatch])
+
+    const searchPost = () => {
+        if (search.trim() || tags) {
+        //   dispatch(getPostsBySearch({ search, tags: tags.join(',') }));
+        //   history.push(`/posts/search?searchQuery=${search || 'none'}&tags=${tags.join(',')}`);
+        } else {
+        //   history.push('/');
+        }
+      };
+
+    const handleKeyPress = (e) => {
+        if (e.keyCode === 13) {
+          searchPost();
+        }
+      };
+
+    const handleAddChip = (tag) => setTags([...tags, tag]);
+    //   *this allows us to delete our tags that we put in the tags serach bar
+    const handleDeleteChip = (tagToDelete) => setTags(tags.filter((tag) => tag !== tagToDelete));
 
     // *<Posts setCurrentId={setCurrentId} /> and <Form currentId={currentId} setCurrentId={setCurrentId} /> this is dealing with prop drilling of the data
         // *this gets sent to the Form.js where we take the props
@@ -42,16 +63,16 @@ const Home = () =>{
             </Grid>
             <Grid item xs={12} sm={6} md={3}>
               <AppBar className={classes.appBarSearch} position="static" color="inherit">
-                <TextField onKeyDown={''} name="search" variant="outlined" label="Search Memories" fullWidth value={'search'} onChange={'(e) => setSearch(e.target.value)'} />
-                {/* <ChipInput
+                <TextField onKeyDown={handleKeyPress} name="search" variant="outlined" label="Search Memories" fullWidth value={search} onChange={(e) => setSearch(e.target.value)} />
+                <ChipInput
                   style={{ margin: '10px 0' }}
                   value={tags}
-                  onAdd={'(chip) => handleAddChip(chip)'}
-                  onDelete={'(chip) => handleDeleteChip(chip)'}
+                  onAdd={(chip) => handleAddChip(chip)}
+                  onDelete={(chip) => handleDeleteChip(chip)}
                   label="Search Tags"
                   variant="outlined"
-                /> */}
-                <Button onClick={'searchPost'} className={classes.searchButton} variant="contained" color="primary">Search</Button>
+                />
+                <Button onClick={searchPost} className={classes.searchButton} variant="contained" color="primary">Search</Button>
               </AppBar>
               <Form currentId={currentId} setCurrentId={setCurrentId} />
               {/* {'(!searchQuery && !tags.length') && (
