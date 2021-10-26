33dbdb0ef3b287c6181b370d81597c18588e3d40
alex <alexhwang01234@gmail.com>
2021-10-26 13:34:22 -0700

    28.  include pagination.js and included it to home.js

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react'
-import { Container, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid, AppBar, TextField, Button, Paper } from '@material-ui/core';
 import Posts from '../Components/Posts/Posts';
 import Form from '../Components/Forms/Form';
 import { useDispatch } from 'react-redux';
 import {getPosts} from '../actions/posts'
+import Pagination from '../Components/Pagination';
 const Home = () =>{
     // *this helps us find our currentId for each post
     const [currentId, setCurrentId] = useState(null);
@@ -27,6 +28,9 @@ const Home = () =>{
                         </Grid>
                         <Grid item xs={12} sm={4}>
                             <Form currentId={currentId} setCurrentId={setCurrentId} />
+                            <Paper elevation={6}>
+                                <Pagination  />
+                            </Paper>
                         </Grid>
                     </Grid>
                 </Container>
--- client/src/Navbar/Navbar.js
+++ client/src/Navbar/Navbar.js
@@ -53,7 +53,7 @@ const Navbar = () =>{
     return(
       <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
-        <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memories; we are good up until 1:51:50</Typography>
+        <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memories</Typography>
         <img className={classes.image} src={memories} alt="icon" height="60" />
       </div>
       <Toolbar className={classes.toolbar}>
