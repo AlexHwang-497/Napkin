9c06ddb3643de59804f8c6437721a402015109af
alex <alexhwang01234@gmail.com>
2021-12-30 09:34:27 -0800

    121.  adjusted width of app in client/app.js;  allowed app to take up whole page

--- client/src/styles.js
+++ client/src/styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
   appBar: {
     borderRadius: 15,
-    margin: '30px 0',
+    margin: '10px 0',
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 // app.use(cors({origin: 'http://localhost:3000'}))
 // ! carlos notes: 
 app.use(cors({origin: ['https://portfoliobuildertool.herokuapp.com','http://localhost:3000']}))
+// app.use(cors({origin: ['http://localhost:3000']}))
 
 
 // *what we have done here is we have set up every route aka postRoutes will start with '/posts'
