0e8f140c5ee7537d7af9c871c13a1a8aab31ad90
alex <alexhwang01234@gmail.com>
2022-02-25 13:27:52 -0800

    168.  updates

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,7 +3,7 @@ import {combineReducers} from 'redux'
 import posts from './posts'
 import auth from './auth'
 import portfolio from './portfolio'
-import SecuritiesPriceReducer from './SecuritiesPriceReducer'
 
 
-export const reducers=combineReducers({posts,auth,SecuritiesPriceReducer,portfolio})
+
+export const reducers=combineReducers({posts,auth,portfolio})
